d9ec54b8f85007775019bcfbf760410b3e22726c
Andrej Mihajlov <and@codeispoetry.ru>
2017-01-27 15:10:28 +0100

    Update and migrate all deps

--- app/client/main.js
+++ app/client/main.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { compose, createStore, combineReducers } from 'redux';
-import { devTools } from 'redux-devtools';
 import { Provider } from 'react-redux';
 import user from './reducers/user';
 import App from './containers/App';
+import DevTools from './containers/DevTools';
+
+const initialState = {};
 
 const isDevelopment = process.env.NODE_ENV === 'development';
-const appCreateStore = isDevelopment ? compose(devTools())(createStore) : createStore;
-const store = appCreateStore(combineReducers({ user }));
+
+const enhancer = isDevelopment ? compose(
+  // Required! Enable Redux DevTools with the monitors you chose
+  DevTools.instrument()
+) : undefined;
+
+const store = createStore(combineReducers({ user }), initialState, enhancer);
 const rootElement = document.querySelector(document.currentScript.getAttribute('data-container'));
 
 ReactDOM.render(
-  <div>
-    <Provider store={store}>
+  <Provider store={store}>
+    <div>
       <App />
-    </Provider>
-    { isDevelopment && do {
-      const { DevTools, DebugPanel, LogMonitor } = require('redux-devtools/lib/react');
-      <DebugPanel top right bottom>
-        <DevTools store={store} monitor={LogMonitor} />
-      </DebugPanel>;
-    } }
-  </div>,
+      { isDevelopment && <DevTools /> }
+    </div>
+  </Provider>,
   rootElement
 );
--- init.js
+++ init.js
@@ -1,15 +1,3 @@
 var path = require('path');
-var cacheDir = path.join(__dirname, 'cache');
 
-if (process.env.NODE_ENV === 'development') {
-  require('electron-compile').initWithOptions({
-    cacheDir: cacheDir,
-    js: {
-      stage: 2
-    }
-  });
-} else {
-  require('electron-compile').initForProduction(cacheDir);
-}
-require('./app/main');
-
+require('electron-compile').init(__dirname, './app/main');
