1d4a1c42387ceefab882fe64d148fb3442416bae
Andrej Mihajlov <and@codeispoetry.ru>
2017-01-30 19:37:02 +0000

    Add redux extension for chrome

--- app/client/main.js
+++ app/client/main.js
@@ -2,27 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { compose, createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import user from './reducers/user';
 import App from './containers/App';
-import DevTools from './containers/DevTools';
-
-const initialState = {};
+import reducers from './reducers';
+import actionCreators from './actions';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
+const initialState = {};
 
-const enhancer = isDevelopment ? compose(
-  // Required! Enable Redux DevTools with the monitors you chose
-  DevTools.instrument()
-) : undefined;
+const composeEnhancers = (() => {
+  const compose_ = window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  if(isDevelopment && compose_) {
+    return compose_({ actionCreators });
+  }
+  return compose;
+})();
 
-const store = createStore(combineReducers({ user }), initialState, enhancer);
+const store = createStore(combineReducers(reducers), initialState, composeEnhancers());
 const rootElement = document.querySelector(document.currentScript.getAttribute('data-container'));
 
 ReactDOM.render(
   <Provider store={store}>
     <div>
       <App />
-      { isDevelopment && <DevTools /> }
     </div>
   </Provider>,
   rootElement
