f9ba969a3fd7d40a1113821c0b7457145775ee02
Steven Garno <SGarno@CarpeDatumInc.com>
2018-06-22 10:10:21 -0600

    Added tests for Logout

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -4,5 +4,8 @@ import actions from '../actions/user';
 export default handleActions({
   [actions.login]: (state, action) => {
     return { ...state, ...action.payload };
+  },
+  [actions.logout]: (state, action) => {
+    return { ...state, ...action.payload };
   }
 }, {});
