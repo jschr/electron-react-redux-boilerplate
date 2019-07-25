6a2a1305df3f40d79761c58ebf2ae32dd2a4952c
akondratsky <akondratsky@yandex.ru>
2019-07-26 00:35:31 +0400

    fixed formatting script

--- nyc.config.js
+++ nyc.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   all: true,
-  exclude: [
-    'dev/**/*.js', 
-    'pre-build/**/*.js',
-    '*.config.js',
-    'coverage',
-    '**/*.spec.js',
-  ],
+  exclude: ['dev/**/*.js', 'pre-build/**/*.js', '*.config.js', 'coverage', '**/*.spec.js'],
   extension: ['.js', '.jsx'],
   reporter: ['text-summary', 'lcov'],
   'temp-dir': 'coverage/.nyc_output',
