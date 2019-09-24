f8827f7780f4e93241ef8af5a64b0bbadf434a78
Andrej Mihajlov <and.mikhaylov@gmail.com>
2019-09-24 14:29:21 +0200

    Allow CSP: unsafe-eval in development because redux-devtools use eval

--- tasks/hotreload.js
+++ tasks/hotreload.js
@@ -23,7 +23,9 @@ function startBrowserSync(done) {
 function injectBrowserSync() {
   return src('app/renderer/index.html')
     .pipe(inject.before('</body>', browserSync.getOption('snippet')))
-    .pipe(inject.after('script-src', ' ' + browserSync.getOption('urls').get('local')))
+    .pipe(
+      inject.after('script-src', " 'unsafe-eval' " + browserSync.getOption('urls').get('local')),
+    )
     .pipe(dest('build/renderer'));
 }
 
